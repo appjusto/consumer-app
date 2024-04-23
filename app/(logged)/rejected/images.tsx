a14988bf4a4538dc85c3a2c36990813da508efd6
Pedro Andrade <pdandradeb@gmail.com>
2024-04-23 19:07:18 -0300

    Rejected: adding profile review after sending images

--- app/(logged)/rejected/images.tsx
+++ app/(logged)/rejected/images.tsx
@@ -1,3 +1,4 @@
+import { useContextApi } from '@/api/ApiContext';
 import { useTrackScreenView } from '@/api/analytics/useTrackScreenView';
 import { DefaultView } from '@/common/components/containers/DefaultView';
 import ProfilePersonalImages from '@/common/screens/profile/images';
@@ -6,20 +7,27 @@ import screens from '@/common/styles/screens';
 import { Stack, router } from 'expo-router';
 
 export default function ProfileRejectedImagesScreen() {
+  // context
+  const api = useContextApi();
   // tracking
   useTrackScreenView('Cadastro reprovado: Selfie e documento');
+  // handlers
+  const updateHandler = () => {
+    api
+      .profile()
+      .requestProfileChange({ images: true })
+      .then(() => {
+        router.replace({
+          pathname: '/(logged)/rejected',
+          params: { uploaded: 'true' },
+        });
+      });
+  };
   // UI
   return (
     <DefaultView style={{ ...screens.default }}>
       <Stack.Screen options={{ title: 'Selfie e documento' }} />
-      <ProfilePersonalImages
-        onUpdateProfile={() => {
-          router.replace({
-            pathname: '/(logged)/rejected',
-            params: { uploaded: 'true' },
-          });
-        }}
-      />
+      <ProfilePersonalImages onUpdateProfile={updateHandler} />
     </DefaultView>
   );
 }
