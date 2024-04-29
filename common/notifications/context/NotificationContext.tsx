9f533596d5c7ee4fd3597e89d7ae8f2d20fe3f5c
Pedro Andrade <pdandradeb@gmail.com>
2024-04-29 12:11:46 -0300

    Removing processing deeplink from notificatoin context

--- common/notifications/context/NotificationContext.tsx
+++ common/notifications/context/NotificationContext.tsx
@@ -1,6 +1,6 @@
+import { trackEvent } from '@/api/analytics/track';
 import { processURL } from '@/common/deeplink/processURL';
 import { PushMessageData } from '@appjusto/types';
-import * as Linking from 'expo-linking';
 import React, { useEffect, useState } from 'react';
 import { useNotificationHandler } from '../useNotificationHandler';
 
@@ -17,20 +17,22 @@ const NotificationContext = React.createContext<Value>({});
 
 export const NotificationProvider = (props: Props) => {
   // state
-  const url = Linking.useURL();
   const [deeplink, setDeeplink] = useState<string>();
   // side effects
   const notification = useNotificationHandler();
   const message = notification ? (notification.request.content.data as PushMessageData) : undefined;
   useEffect(() => {
     if (!message) return;
-    if (message.action === 'navigate' || message.action === 'order-request') {
-      setDeeplink(message.url);
+    if (
+      message.action === 'navigate' ||
+      message.action === 'order-request' ||
+      message.action === 'order-chat'
+    ) {
+      trackEvent('Clicou no push', { url: message.url });
+      const result = processURL(message.url);
+      if (result) setDeeplink(result);
     }
   }, [message]);
-  useEffect(() => {
-    if (url) setDeeplink(processURL(url));
-  }, [url]);
   // result
   return (
     <NotificationContext.Provider value={{ deeplink, setDeeplink }}>
