d0aaca8d2bdedea78b0fce96389d345abb87b186
Pedro Andrade <pdandradeb@gmail.com>
2024-05-14 13:22:51 -0300

    Fixing complement flow error

--- app/(logged)/places/complement.tsx
+++ app/(logged)/places/complement.tsx
@@ -1,8 +1,12 @@
 import { NewPlaceComplement } from '@/common/screens/places/complement/complement';
 import { useGlobalSearchParams } from 'expo-router';
+import { isEmpty } from 'lodash';
 
 export default function NewPlaceComplementScreen() {
   const params = useGlobalSearchParams<{ returnScreen?: string }>();
-  const returnScreen = params.returnScreen ?? '/(logged)/(tabs)/(home)/';
+  const returnScreen = isEmpty(params.returnScreen)
+    ? '/(logged)/(tabs)/(home)/'
+    : (params.returnScreen as string);
+  console.log('NewPlaceComplementScreen', returnScreen);
   return <NewPlaceComplement returnScreen={returnScreen} />;
 }
